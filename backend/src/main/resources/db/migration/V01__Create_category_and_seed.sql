0c8f9e9c44dd743300191dcc28c64b176bd85b62
Arthur Cech Francisco <arthurcech@gmail.com>
2023-02-23 20:00:17 -0300

    refactor(category): add createdAt and updatedAt on migration V01

--- backend/src/main/resources/db/migration/V01__Create_category_and_seed.sql
+++ backend/src/main/resources/db/migration/V01__Create_category_and_seed.sql
@@ -1,8 +1,10 @@
 CREATE TABLE tb_category (
     id BIGSERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 );
 
-INSERT INTO tb_category (name) VALUES ('Books');
-INSERT INTO tb_category (name) VALUES ('Electronics');
-INSERT INTO tb_category (name) VALUES ('Computers');
+INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Books', NOW(), NOW());
+INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Electronics', NOW(), NOW());
+INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Computers', NOW(), NOW());
