1525f667a205054d2568cbcd7a0ebe83168c595b
Arthur Cech Francisco <arthurcech@gmail.com>
2023-02-23 19:59:36 -0300

    refactor(category): add findByName in repository

--- backend/src/main/java/com/github/arthurcech/productcatalog/repository/CategoryRepository.java
+++ backend/src/main/java/com/github/arthurcech/productcatalog/repository/CategoryRepository.java
@@ -3,5 +3,10 @@ package com.github.arthurcech.productcatalog.repository;
 import com.github.arthurcech.productcatalog.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
+
 }
