c530ab0b917e63b55e504b569a5577f92b6ee9a6
Arthur Cech Francisco <arthurcech@gmail.com>
2023-02-24 09:23:22 -0300

    feat: add Product migration to create table and seed

--- backend/src/main/resources/db/migration/V01__Create_category_and_seed.sql
+++ backend/src/main/resources/db/migration/V01__Create_category_and_seed.sql
@@ -1,10 +1,10 @@
 CREATE TABLE tb_category (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
 
-INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Books', NOW(), NOW());
-INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Electronics', NOW(), NOW());
-INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Computers', NOW(), NOW());
+INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Livros', NOW(), NOW());
+INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Eletrônicos', NOW(), NOW());
+INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Computadores', NOW(), NOW());
